3a54d65a127da0df92238010cc86575a4793baf1
Julio Vergara <gogczoro@gmail.com>
2024-04-17 17:03:06 -0600

    Implement the cache for local data

--- atomic_design_system/atomic_design_system_app/lib/src/features/student_home_page/data/datasources/session/session_local_data_source.dart
+++ atomic_design_system/atomic_design_system_app/lib/src/features/student_home_page/data/datasources/session/session_local_data_source.dart
@@ -1,16 +1,40 @@
+import 'dart:convert';
+
 import 'package:shared_preferences/shared_preferences.dart';
 
+import '../../../../../../core/error/exception.dart';
 import '../../models/session_model.dart';
-import '../../models/student_today_model.dart';
 
 abstract class SessionLocalDataSource {
-  final SharedPreferences sharedPreferences;
-
-  SessionLocalDataSource({required this.sharedPreferences});
-
   Future<SessionModel> getLastSession();
   Future<void> cacheSession(SessionModel sessionToCache);
+}
 
-  Future<StudentTodayModel> getLastStudentToday();
-  Future<void> cacheStudentToday(StudentTodayModel studentTodayToCache);
+const CACHED_SESSION = 'CACHED_SESSION';
+
+class SessionLocalDataSourceImpl implements SessionLocalDataSource {
+  final SharedPreferences sharedPreferences;
+
+  SessionLocalDataSourceImpl({required this.sharedPreferences});
+
+  @override
+  Future<SessionModel> getLastSession() {
+    final jsonString = sharedPreferences.getString(CACHED_SESSION);
+// este método declara que retorna un futuro, sin embargo getString nos arroja
+// un valor de tipo String, por lo que no es necesario usar un wrap de Future.value()
+// para retornar el tipo que necesitamos.
+    if (jsonString != null) {
+      return Future.value(SessionModel.fromJSON(json.decode(jsonString)));
+    } else {
+      throw CacheException();
+    }
+  }
+
+  @override
+  Future<void> cacheSession(SessionModel sessionToCache) {
+    return sharedPreferences.setString(
+      CACHED_SESSION,
+      json.encode(sessionToCache.toJSON()),
+    );
+  }
 }
